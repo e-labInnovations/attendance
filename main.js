a7b9b38272fac6360e2a7e9967f49ac276e11399
e-labinnovations <mohammedashad23@gmail.com>
2021-01-19 19:26:16 +0530

    Finished

--- main.js
+++ main.js
@@ -4,8 +4,13 @@ const inputRollNo = document.getElementById('rollNo');
 const inputName = document.getElementById('name');
 const avatar = document.getElementById('avatar');
 const title = document.querySelector('.title')
+const form = document.getElementById('form');
+const submit = document.getElementById('submit');
+const msg = document.getElementById('msg');
 
-var student = {};
+var isOpen = true;
+var student = { status: false };
+var subject = null;
 const api = "https://script.google.com/macros/s/AKfycbyGwfwpGj18FB9kbLo4J-zml08bxTgWfLquMdoo6h6m2PwaXQsnkuUy7w/exec";
 
 const CancelToken = axios.CancelToken;
@@ -43,15 +48,36 @@ const validateRollNo = () => {
 }
 
 const addAttendance = () => {
-  let regNo = inputRegNo.value;
-  let rollNo = inputRollNo.value;
-  if (regNo.length !== 8 || rollNo.length == 0 || rollNo.length > 2) {
-    showModal("Error", "Please enter correct Register No and Roll Number")
+  if (!student.status) {
+    showModal("Error", "Student not found please fill the form and wait for student name", 'error')
   } else {
-    if (!(regNo && rollNo)) {
-      localStorage.setItem('regNo', regNo);
-      localStorage.setItem('rollNo', rollNo);
+    if (!(localStorage.getItem('regNo') && localStorage.getItem('rollNo'))) {
+      localStorage.setItem('regNo', student.regNo);
+      localStorage.setItem('rollNo', student.rollNo);
     }
+
+    submit.classList.add('is-loading');
+    let url = `${api}?action=addAttendance&subject=${subject}&regNo=${student.regNo}&rollNo=${student.rollNo}`;
+    axios.get(url)
+      .then(function(response) {
+        submit.classList.remove('is-loading');
+        localStorage.setItem(subject, true);
+        submit.remove();
+        msg.innerHTML = `
+                      <p class="has-text-grey">
+                        You are submitted your attendance.
+                      </p>
+                      `
+        if (response.data.success) {
+          showModal('Success', response.data.message, 'success');
+        } else {
+          showModal('Error', response.data.message, 'error');
+        }
+      })
+      .catch(function(error) {
+        submit.classList.remove('is-loading');
+        showModal("Error", error, 'error')
+      });
   }
 }
 
@@ -77,17 +103,39 @@ const setStudent = (regNo, rollNo) => {
         inputName.value = student.name;
         avatar.src = 'https://ui-avatars.com/api/?size=128&background=random&length=1&name=' + student.name;
       } else {
-        showModal("Error", response.data.message);
+        showModal("Error", response.data.message, 'error');
       }
     })
     .catch(function(error) {
       //inputName.parentNode.classList.remove('is-loading');
       //inputName.placeholder = 'Name';
       if (axios.isCancel(error)) {
-        console.log("post Request canceled");
+        console.log("get Request canceled");
       } else {
-        showModal("Error", error)
+        showModal("Error", error, 'error')
       }
+    });
+}
+
+const checkIsOpen = (subject) => {
+  let url = `${api}?action=isOpen&subject=${subject}`;
+  axios.get(url)
+    .then(function(response) {
+      isOpen = response.data.isOpen;
+      if (!isOpen) {
+        submit.remove();
+        msg.innerHTML = `
+        <p class="has-text-grey">
+          The form ${response.data.subject} is no longer accepting new attendance.
+          Try contacting <span class="has-text-danger"> Shivani </span> if you think this is a mistake.
+        </p>
+        `
+      } else {
+        submit.disabled = false;
+      }
+    })
+    .catch(function(error) {
+      showModal("Error", error, 'error')
     });
 }
 
@@ -96,6 +144,13 @@ const showModal = (title, message, status) => {
   let modalCardBody = document.querySelector('.modal-card-body');
   modalCardTitle.innerHTML = title;
   modalCardBody.innerHTML = message;
+  if (status == 'error') {
+    modal.classList.remove('has-background-success')
+    modal.classList.add('has-background-danger')
+  } else if (status == 'success') {
+    modal.classList.add('has-background-success')
+    modal.classList.remove('has-background-danger')
+  }
   modal.classList.add('is-active');
 }
 
@@ -123,17 +178,31 @@ const getParameter = () => {
   let parameter = getParameter();
   let regNo = localStorage.getItem('regNo');
   let rollNo = localStorage.getItem('rollNo');
-  
-  if(parameter.subject){
-    title.innerText = parameter.subject;
-  
-  if (regNo && rollNo) {
-    inputRegNo.value = regNo;
-    inputRollNo.value = rollNo;
-    inputRegNo.disabled = true;
-    inputRollNo.disabled = true;
-    setStudent(regNo, rollNo);
+
+  if (parameter.subject) {
+    subject = parameter.subject;
+    title.innerText = subject;
+    checkIsOpen(subject);
+
+    if (localStorage.getItem(subject)) {
+      submit.remove();
+      msg.innerHTML = `
+              <p class="has-text-grey">
+                You already submitted your attendance.
+                Try contacting <span class="has-text-danger"> Shivani </span> if you think this is a mistake.
+              </p>
+              `
+    }
+
+    if (regNo && rollNo) {
+      inputRegNo.value = regNo;
+      inputRollNo.value = rollNo;
+      inputRegNo.disabled = true;
+      inputRollNo.disabled = true;
+      setStudent(regNo, rollNo);
+    }
+  } else {
+    console.log('No subject')
   }
-  } else {}
-  
+
 })();
