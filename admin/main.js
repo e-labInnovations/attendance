7b7809af76246c2c4f6b8569f2a255e95883eb60
e-labinnovations <mohammedashad23@gmail.com>
2021-01-20 17:47:45 +0530

    try to fix

--- admin/main.js
+++ admin/main.js
@@ -107,9 +107,7 @@ const getParameter = () => {
   return obj;
 }
 
-// The following code is based off a toggle menu by @Bradcomp
-// source: https://gist.github.com/Bradcomp/a9ef2ef322a8e8017443b626208999c1
-(function() {
+
   var burger = document.querySelector('.burger');
   var menu = document.querySelector('#' + burger.dataset.target);
   burger.addEventListener('click', function() {
@@ -134,4 +132,3 @@ const getParameter = () => {
     }
     window.location.replace('./login');
   }
-})();
